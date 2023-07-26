ff1aee401e4f5652b633b01f1e1350e30971c47d
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-07-26 22:38:33 +0300

    fix: when access token is expired, refres token is also deleted. only access should be deleted

--- packages/api/src/common/guards/refreshToken.guard.ts
+++ packages/api/src/common/guards/refreshToken.guard.ts
@@ -1,7 +1,20 @@
+import { ForbiddenException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 export class RefreshTokenGuard extends AuthGuard('jwt-refresh') {
   constructor() {
     super();
+  }
+
+  handleRequest(err, user, _info, context) {
+    if (err || !user) {
+      const response = context.switchToHttp().getResponse();
+      response.clearCookie('accessToken');
+      response.clearCookie('refreshToken');
+
+      throw err || new ForbiddenException();
+    }
+
+    return user;
   }
 }
