99eb5161aad491b17eb755a29447b56c20f1a945
Salih <salihozdemir94@gmail.com>
2023-10-22 17:30:46 +0300

    chore: add eslint and prettier

--- packages/widget-core/packages/lib/api/index.ts
+++ packages/widget-core/packages/lib/api/index.ts
@@ -1,23 +1,27 @@
 import type { IFeedback } from "../types";
 
 export const sendFeedback = async (feedback: IFeedback) => {
-  try {
-    const response = await fetch("http://localhost:3001/feedbacks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(feedback),
-    });
+	try {
+		const response = await fetch("http://localhost:3001/feedbacks", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(feedback)
+		});
 
-    const body = await response.json();
+		const body = await response.json();
 
-    if (!response.ok) {
-      throw new Error(body.message);
-    }
+		if (!response.ok) {
+			throw new Error(body.message);
+		}
 
-    return body;
-  } catch (error: any) {
-    throw new Error(error);
-  }
+		return body;
+	} catch (error) {
+		if (error instanceof Error) {
+			throw new Error(error.message);
+		} else {
+			throw new Error("An unknown error occurred.");
+		}
+	}
 };
--- packages/widget-core/packages/lib/components/icons/other.wc.svelte
+++ packages/widget-core/packages/lib/components/icons/other.wc.svelte
@@ -1,13 +1,6 @@
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="48"
-  height="48"
-  fill="none"
-  viewBox="0 0 48 48"
-  ><circle cx="9.5" cy="24.5" r="3.5" fill="#2D3748" /><circle
-    cx="23.5"
-    cy="24.5"
-    r="3.5"
-    fill="#2D3748"
-  /><circle cx="37.5" cy="24.5" r="3.5" fill="#2D3748" /></svg
->
+<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="none" viewBox="0 0 48 48"
+	><circle cx="9.5" cy="24.5" r="3.5" fill="#2D3748" /><circle
+		cx="23.5"
+		cy="24.5"
+		r="3.5"
+		fill="#2D3748" /><circle cx="37.5" cy="24.5" r="3.5" fill="#2D3748" /></svg>
