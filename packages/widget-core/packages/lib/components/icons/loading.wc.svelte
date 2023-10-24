4a787c7e155140e40f15f5ce7455c2a6ee886f11
Salih <salihozdemir94@gmail.com>
2023-10-24 15:06:45 +0300

    feat: add form step view

---
+++ packages/widget-core/packages/lib/components/icons/loading.wc.svelte
@@ -0,0 +1,27 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="16"
+	height="16"
+	fill="none"
+	stroke="currentColor"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	stroke-width="2"
+	class="icon-loading"
+	viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z" /><path d="M12 3a9 9 0 1 0 9 9" /></svg
+>
+
+<style>
+	.icon-loading {
+		animation: spin 1s linear;
+	}
+
+	@keyframes spin {
+		from {
+			transform: rotate(0deg);
+		}
+		to {
+			transform: rotate(360deg);
+		}
+	}
+</style>
