56613c2cb3f711f9cedaf255eba3b442d99e9a51
Salih <salihozdemir94@gmail.com>
2023-10-31 12:58:38 +0300

    fix: update the react package props

--- packages/widget-react/src/feedbacky.tsx
+++ packages/widget-react/src/feedbacky.tsx
@@ -1,10 +1,17 @@
-"use client"
+"use client";
 
 import React from "react";
 import "@feedbacky/widget-core";
 
 export interface FeedbackyProps {
   projectId: string;
+  theme?: {
+    scheme?: "light" | "dark";
+    primaryColor?: string;
+    backgroundColor?: string;
+    textColor?: string;
+  };
+  position?: "left" | "right";
 }
 
 declare global {
@@ -12,11 +19,25 @@ declare global {
     interface IntrinsicElements {
       "feedbacky-widget": {
         "project-id": string;
+        "theme-scheme"?: "light" | "dark";
+        "theme-primary-color"?: string;
+        "theme-background-color"?: string;
+        "theme-text-color"?: string;
+        position?: "left" | "right";
       };
     }
   }
 }
 
-export const Feedbacky = ({ projectId }: FeedbackyProps) => {
-  return <feedbacky-widget project-id={projectId}></feedbacky-widget>;
+export const Feedbacky = ({ projectId, position, theme }: FeedbackyProps) => {
+  return (
+    <feedbacky-widget
+      project-id={projectId}
+      position={position}
+      theme-scheme={theme?.scheme}
+      theme-primary-color={theme?.primaryColor}
+      theme-background-color={theme?.backgroundColor}
+      theme-text-color={theme?.textColor}
+    />
+  );
 };
