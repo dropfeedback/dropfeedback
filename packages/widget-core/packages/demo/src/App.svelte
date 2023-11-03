1a640a6a31b438ae760b79f618b07e6dcbd74440
Salih <salihozdemir94@gmail.com>
2023-11-03 12:32:01 +0300

    feat: create playground for widget

--- packages/widget-core/packages/demo/src/App.svelte
+++ packages/widget-core/packages/demo/src/App.svelte
@@ -1,24 +1,74 @@
 <script lang="ts">
 	import "../../lib";
 
-	let darkMode = false;
+	let themeScheme = "light";
 	let position = "right";
+	let primaryColor = "#1677ff";
+	let textColor = "#171717";
+	let backgroundColor = "#fafafa";
+
+	$: {
+		document.documentElement.classList.toggle("dark", themeScheme === "dark");
+	}
+
+	$: {
+		if (themeScheme === "light") {
+			textColor = "#171717";
+			backgroundColor = "#fafafa";
+		} else {
+			textColor = "#ededed";
+			backgroundColor = "#000000";
+		}
+	}
 </script>
 
 <main>
-	<h1>Hello World!</h1>
-	<h2>DarkMode: {darkMode ? "On" : "Off"}</h2>
-	<button on:click={() => (darkMode = !darkMode)}> Toggle Dark Mode </button>
-	<button on:click={() => (position === "right" ? (position = "left") : (position = "right"))}>
-		Toggle Position
-	</button>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
-		apps.
-	</p>
+	<fieldset>
+		<legend>Theme:</legend>
+
+		<div>
+			<input type="radio" id="light" name="theme" value="light" checked bind:group={themeScheme} />
+			<label for="light">Light</label>
+
+			<input type="radio" id="dark" name="theme" value="dark" bind:group={themeScheme} />
+			<label for="dark">Dark</label>
+		</div>
+
+		<div>
+			<div>
+				<input type="color" id="primaryColor" bind:value={primaryColor} />
+				<label for="primaryColor">Primary Color</label>
+			</div>
+
+			<div>
+				<input type="color" id="backgroundColor" bind:value={backgroundColor} />
+				<label for="backgroundColor">Background Color</label>
+
+				<input type="color" id="textColor" bind:value={textColor} />
+				<label for="textColor">Text Color</label>
+			</div>
+		</div>
+	</fieldset>
+	<fieldset>
+		<legend>Positon:</legend>
+
+		<div>
+			<input type="radio" id="right" name="position" value="right" checked bind:group={position} />
+			<label for="right">Right</label>
+
+			<input type="radio" id="left" name="position" value="left" bind:group={position} />
+			<label for="left">Left</label>
+		</div>
+	</fieldset>
+
+	<img class={`arrow arrow-${position} arrow-${themeScheme}`} src="/arrow.svg" alt="Arrow" />
+
 	<feedbacky-widget
 		project-id="24320d3d-c5f2-44e1-83a1-13e1cb61fe1d"
-		theme-scheme={darkMode ? "dark" : "light"}
+		theme-scheme={themeScheme}
+		theme-primary-color={primaryColor}
+		theme-text-color={textColor}
+		theme-background-color={backgroundColor}
 		{position}
 	/>
 </main>
