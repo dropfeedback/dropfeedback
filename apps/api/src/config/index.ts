9ae81dc2025f423d903393142de125e2eac26be1
melihkizmaz <melihkizmaz@icloud.com>
2023-11-05 01:30:50 +0300

    feat: EMAIL_TOKEN_SECRET and EMAIL_TOKEN_EXPIRES_IN envorinment added to config

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -11,7 +11,9 @@ export const config = () => {
     DATABASE_URL: throwEnvError('DATABASE_URL'),
     ACCESS_TOKEN_SECRET: throwEnvError('ACCESS_TOKEN_SECRET'),
     REFRESH_TOKEN_SECRET: throwEnvError('REFRESH_TOKEN_SECRET'),
+    EMAIL_TOKEN_SECRET: throwEnvError('EMAIL_TOKEN_SECRET'),
     ACCESS_TOKEN_EXPIRES_IN: 15 * 60 * 1000, // 15 minutes
     REFRESH_TOKEN_EXPIRES_IN: 7 * 24 * 60 * 60 * 1000, // 7 days
+    EMAIL_TOKEN_EXPIRES_IN: 15 * 24 * 60 * 60 * 1000, // 15 days
   } as const;
 };
