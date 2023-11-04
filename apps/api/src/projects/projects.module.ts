b047356c0812924b1ffab6d164fb78ae32a56aa8
melihkizmaz <melihkizmaz@icloud.com>
2023-11-05 01:23:20 +0300

    feat: add member method created for project service

--- apps/api/src/projects/projects.module.ts
+++ apps/api/src/projects/projects.module.ts
@@ -3,8 +3,10 @@ import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { AccessTokenStrategy } from 'src/auth/strategies';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule.register({})],
   providers: [ProjectsService, AccessTokenStrategy, PrismaService],
   controllers: [ProjectsController],
 })
