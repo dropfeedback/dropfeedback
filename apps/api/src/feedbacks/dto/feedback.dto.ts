db6674c30db94285b29d02213ec32a9e6a22f354
melihkizmaz <melihkizmaz@icloud.com>
2023-11-05 23:43:42 +0300

    chore: unused imports removed

--- apps/api/src/feedbacks/dto/feedback.dto.ts
+++ apps/api/src/feedbacks/dto/feedback.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  IsUUID,
-  MinLength,
-} from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { Feedback, Prisma } from 'src/prisma';
 
 export class FeedbackDto
--- apps/api/src/feedbacks/feedbacks.service.ts
+++ apps/api/src/feedbacks/feedbacks.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FeedbackDto } from './dto';
 
