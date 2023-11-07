f4d1e66ca73b5ffbe82a3bdad5a8a5d85c78c055
Salih <salihozdemir94@gmail.com>
2023-11-07 15:13:17 +0300

    feat: add basic routing logic

--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -3,8 +3,20 @@ export default {
   ignoredRouteFiles: ["**/.*"],
   tailwind: true,
   postcss: true,
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildPath: "build/index.js",
+  routes(defineRoutes) {
+    return defineRoutes((route) => {
+      route("/", "routes/landing/route.tsx", { index: true });
+      route("docs", "routes/docs/route.tsx");
+      route("dashboard", "routes/dashboard/layout.tsx", () => {
+        route("", "routes/dashboard/route.tsx", { index: true });
+        route(":projectId", "routes/dashboard/project/layout.tsx", () => {
+          route("", "routes/dashboard/project/route.tsx", { index: true });
+        });
+      });
+    });
+  },
 };
