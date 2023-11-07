757f804d4d1acab5f057d405c7cb185ad3c195de
Salih <salihozdemir94@gmail.com>
2023-11-07 10:14:44 +0300

    chore: remove old dashboard project

--- apps/dashboard/src/components/dashboard-header/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC, PropsWithChildren } from "react";
-
-export const DashboardHeader: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <div className="border-b">
-      <div className="flex items-center justify-between p-4">{children}</div>
-    </div>
-  );
-};
