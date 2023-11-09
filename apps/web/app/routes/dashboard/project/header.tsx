f015648565c957d82f5dd5dcbd1e266803df3465
Salih <salihozdemir94@gmail.com>
2023-11-09 16:53:58 +0300

    feat: add animation to show logo on project header

--- apps/web/app/routes/dashboard/project/header.tsx
+++ apps/web/app/routes/dashboard/project/header.tsx
@@ -1,4 +1,7 @@
+import { ChatBubbleIcon } from "@radix-ui/react-icons";
 import { Link, useLocation, useParams } from "@remix-run/react";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import { Button } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
 
@@ -26,8 +29,25 @@ const ROUTES = [
 ];
 
 export function Header() {
+  const [isStickyActive, setIsStickyActive] = useState(false);
   const params = useParams<{ projectId: string }>();
   const location = useLocation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 45) {
+        setIsStickyActive(true);
+      } else {
+        setIsStickyActive(false);
+      }
+    };
+
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const activeRoute =
     ROUTES.find((route) => {
@@ -35,22 +55,40 @@ export function Header() {
     }) ?? ROUTES[0];
 
   return (
-    <div className="sticky top-0 -ml-3 -mt-3 flex h-auto rounded-none border-b border-muted bg-background px-6 py-2 text-muted-foreground shadow-sm">
-      {ROUTES.map((route) => (
-        <Button
-          key={route.title}
-          variant="ghost"
-          size="default"
-          asChild
-          className={cn("relative font-normal", {
-            "active-tab text-primary": activeRoute.key === route.key,
-          })}
+    <div
+      className={
+        "sticky top-0 -mt-3 flex h-auto items-center rounded-none border-b border-muted bg-background px-6 py-2 text-muted-foreground shadow-sm"
+      }
+    >
+      {isStickyActive && (
+        <motion.div
+          initial={{ opacity: 0, y: -40 }}
+          animate={{ opacity: 1, y: 0 }}
         >
-          <Link to={route.path.replace(":projectId", params.projectId!)}>
-            {route.title}
-          </Link>
-        </Button>
-      ))}
+          <ChatBubbleIcon className="h-5 w-5" />
+        </motion.div>
+      )}
+      <motion.div
+        layout
+        layoutDependency={isStickyActive}
+        className={cn("relative ml-2", { "-ml-3": !isStickyActive })}
+      >
+        {ROUTES.map((route) => (
+          <Button
+            key={route.key}
+            variant="ghost"
+            size="default"
+            asChild
+            className={cn("relative font-normal", {
+              "active-tab text-primary": activeRoute.key === route.key,
+            })}
+          >
+            <Link to={route.path.replace(":projectId", params.projectId!)}>
+              {route.title}
+            </Link>
+          </Button>
+        ))}
+      </motion.div>
     </div>
   );
 }
