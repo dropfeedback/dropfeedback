c784bd5ef9243df9c76efcad7932f6211382a4c7
melihkizmaz <melihkizmaz@icloud.com>
2023-11-10 19:30:20 +0300

    feat: email tokens added to config

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -8,10 +8,14 @@ const throwEnvError = (key: string) => {
 export const config = () => {
   return {
     port: parseInt(process.env.PORT || '3001', 10),
+    APP_URL: throwEnvError('APP_URL'),
     DATABASE_URL: throwEnvError('DATABASE_URL'),
     ACCESS_TOKEN_SECRET: throwEnvError('ACCESS_TOKEN_SECRET'),
     REFRESH_TOKEN_SECRET: throwEnvError('REFRESH_TOKEN_SECRET'),
     EMAIL_TOKEN_SECRET: throwEnvError('EMAIL_TOKEN_SECRET'),
+    CLIENT_SECRET: throwEnvError('CLIENT_SECRET'),
+    REFRESH_TOKEN: throwEnvError('REFRESH_TOKEN'),
+    EMAIL: throwEnvError('EMAIL'),
     ACCESS_TOKEN_EXPIRES_IN: 24 * 60 * 60 * 1000, // 1 day
     REFRESH_TOKEN_EXPIRES_IN: 7 * 24 * 60 * 60 * 1000, // 7 days
     EMAIL_TOKEN_EXPIRES_IN: 15 * 24 * 60 * 60 * 1000, // 15 days
