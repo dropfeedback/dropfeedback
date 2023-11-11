22c5e3c2bdeaf0f5a55871724467d73926e99672
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-11 04:20:10 +0300

    feat(api): add pagination to feedback

--- apps/api/src/feedbacks/feedbacks.service.ts
+++ apps/api/src/feedbacks/feedbacks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FeedbackDto } from './dto';
+import { CursorPagination, OrderBy } from 'src/common/types';
 
 @Injectable()
 export class FeedbacksService {
@@ -9,23 +10,38 @@ export class FeedbacksService {
   async getAllByProjectId({
     projectId,
     search,
+    pagination,
+    orderBy,
   }: {
     projectId: string;
-    userId: string;
     search?: string;
+    pagination?: CursorPagination;
+    orderBy?: OrderBy;
   }) {
-    return this.prisma.feedback.findMany({
-      orderBy: {
-        createdAt: 'desc',
-      },
-      where: {
-        projectId,
-        content: {
-          contains: search,
-          mode: 'insensitive',
+    const [data, total] = await this.prisma.$transaction([
+      this.prisma.feedback.findMany({
+        where: {
+          projectId,
+          content: {
+            contains: search,
+            mode: 'insensitive',
+          },
         },
-      },
-    });
+        orderBy,
+        ...pagination,
+      }),
+      this.prisma.feedback.count({ where: { projectId } }),
+    ]);
+
+    const nextCursor = data[data.length - 1]?.id;
+    const prevCursor = data[0]?.id;
+
+    return {
+      data,
+      total,
+      nextCursor,
+      prevCursor,
+    };
   }
 
   async createByProjectId({
