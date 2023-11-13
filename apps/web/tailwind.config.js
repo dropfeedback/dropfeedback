2690e26a8f4b7a086c96484d0c48924ea25d74fa
Salih <salihozdemir94@gmail.com>
2023-11-13 17:26:05 +0300

    chore: add link color

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        link: "#0070f3",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
