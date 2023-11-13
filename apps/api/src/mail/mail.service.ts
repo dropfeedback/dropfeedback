d936fef511bd3c8652382479a7a65b14c0d2ea10
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-13 21:33:27 +0300

    Revert "feat(api): remove mail service"
    This reverts commit e80c85b3573548400e5cb43447b425769032bcc1.

---
+++ apps/api/src/mail/mail.service.ts
@@ -0,0 +1,90 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { render } from '@react-email/render';
+import { google } from 'googleapis';
+import { Options } from 'nodemailer/lib/smtp-transport';
+import { InviteEmail } from 'src/mail/mails/invite-email';
+
+@Injectable()
+export class MailService {
+  constructor(
+    private readonly config: ConfigService,
+    private readonly mailerService: MailerService,
+  ) {}
+
+  private async setTransport() {
+    const OAuth2 = google.auth.OAuth2;
+    const oauth2Client = new OAuth2(
+      this.config.get<string>('CLIENT_ID'),
+      this.config.get<string>('CLIENT_SECRET'),
+      'https://developers.google.com/oauthplayground',
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN,
+    });
+
+    const accessToken: string = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject('Failed to create access token');
+        }
+
+        if (token) {
+          resolve(token);
+        } else {
+          reject('Failed to create access token');
+        }
+      });
+    });
+
+    const config: Options = {
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: this.config.get<string>('EMAIL'),
+        clientId: this.config.get<string>('CLIENT_ID'),
+        clientSecret: this.config.get<string>('CLIENT_SECRET'),
+        accessToken,
+      },
+    };
+    this.mailerService.addTransporter('gmail', config);
+  }
+
+  private async sendMail({
+    subject,
+    email,
+    html,
+  }: {
+    subject: 'Verification email';
+    email: string;
+    html: string;
+  }) {
+    try {
+      await this.setTransport();
+
+      await this.mailerService.sendMail({
+        transporterName: 'gmail',
+        to: email,
+        from: this.config.get<string>('EMAIL'),
+        subject,
+        html,
+      });
+    } catch (error) {
+      //TODO: add logger
+      console.log(error);
+    }
+  }
+
+  async sendInviteEmail({
+    email,
+    projectName,
+  }: {
+    email: string;
+    projectName: string;
+  }) {
+    const html = render(InviteEmail({ projectName }));
+    this.sendMail({ subject: 'Verification email', email, html });
+  }
+}
