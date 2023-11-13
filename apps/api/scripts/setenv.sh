083261dc88cc8c7fbfdc4940aeb2f422fcf62cf7
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-13 21:53:44 +0300

    fix(api): lad env

--- apps/api/scripts/setenv.sh
+++ apps/api/scripts/setenv.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # scripts/setenv.sh
 
+# Copy file to current dir and rename it
+cp  .env.test .env
 # Export env vars
-export $(grep -v '^#' .env.test | xargs)
+export $(grep -v '^#' .env | xargs)
