9d91ddb136fc81990ba79662e105e9e34ea50694
Salih <salihozdemir94@gmail.com>
2023-11-13 17:38:28 +0300

    chore: update the local api URL

--- apps/web/app/lib/axios.ts
+++ apps/web/app/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "http://localhost:8080";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
