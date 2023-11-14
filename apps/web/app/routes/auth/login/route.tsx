de5457334ea8bbc586c132121b113497dceea2e8
Salih <salihozdemir94@gmail.com>
2023-11-14 17:41:53 +0300

    feat: add legal terms

--- apps/web/app/routes/auth/login/route.tsx
+++ apps/web/app/routes/auth/login/route.tsx
@@ -4,8 +4,8 @@ import { Separator } from "~/components/ui/separator";
 
 export default function Login() {
   return (
-    <div>
-      <h1 className="mb-10 text-3xl font-semibold tracking-tight">
+    <div className="space-y-10">
+      <h1 className="text-center text-3xl font-semibold tracking-tight">
         Log in to DropFeedback
       </h1>
       <div className="m-auto flex w-min flex-col items-center">
--- apps/web/app/routes/auth/signup/route.tsx
+++ apps/web/app/routes/auth/signup/route.tsx
@@ -1,11 +1,12 @@
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
 import { Link } from "@remix-run/react";
 import GoogleOAuthButton from "~/components/google-oauth-button";
 import { Separator } from "~/components/ui/separator";
 
 export default function Signup() {
   return (
-    <div>
-      <h1 className="mb-10 text-3xl font-semibold tracking-tight">
+    <div className="space-y-10">
+      <h1 className="text-center text-3xl font-semibold tracking-tight">
         Sign up to DropFeedback
       </h1>
       <div className="m-auto flex w-min flex-col items-center">
@@ -15,6 +16,18 @@ export default function Signup() {
           Continue with Email →
         </Link>
       </div>
+      <p className="text-sm font-light text-muted-foreground">
+        By joining, you agree to our{" "}
+        <Link to="#" className="font-semibold text-primary hover:underline">
+          Terms of Service
+          <ExternalLinkIcon className="ml-0.5 inline-block h-4 w-4" />
+        </Link>{" "}
+        and{" "}
+        <Link to="#" className="font-semibold text-primary hover:underline">
+          Privacy Policy
+          <ExternalLinkIcon className="ml-0.5 inline-block h-4 w-4" />
+        </Link>
+      </p>
     </div>
   );
 }
