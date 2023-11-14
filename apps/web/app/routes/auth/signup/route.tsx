ebeade09037ac3bb0e93a89cc993cb5427a21a26
Salih <salihozdemir94@gmail.com>
2023-11-14 16:52:59 +0300

    feat: add auth header

--- apps/web/app/routes/auth/signup/route.tsx
+++ apps/web/app/routes/auth/signup/route.tsx
@@ -4,19 +4,17 @@ import { Separator } from "~/components/ui/separator";
 
 export default function Signup() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col">
-        <h1 className="text-2xl font-semibold tracking-tight">
-          Sign up for DropFeedback
-        </h1>
-        <br />
-        <GoogleOAuthButton />
-        <Separator className="my-6" />
-        <div className="text-center">
-          <Link to="/signup/email" className="text-base text-link">
-            Continue with Email →
-          </Link>
-        </div>
+    <div className="flex flex-col">
+      <h1 className="text-2xl font-semibold tracking-tight">
+        Sign up for DropFeedback
+      </h1>
+      <br />
+      <GoogleOAuthButton />
+      <Separator className="my-6" />
+      <div className="text-center">
+        <Link to="/signup/email" className="text-base text-link">
+          Continue with Email →
+        </Link>
       </div>
     </div>
   );
