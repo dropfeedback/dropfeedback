4e5a1ecd1aa090ae6b16cff399bfe3b392b4a08f
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-15 22:04:37 +0300

    fix(workflos): run e2e test

--- apps/api/scripts/run-integration.sh
+++ apps/api/scripts/run-integration.sh
@@ -1,27 +1,16 @@
 #!/usr/bin/env bash
 # src/run-integration.sh
 
-# Get the directory of this script
-DIR="$(cd "$(dirname "$0")" && pwd)"
-echo "$PWD"
 # Export env vars
 export $(grep -v '^#' .env.test | xargs)
 
-# go to the root of the project
-cd ..
-echo "$PWD"
 # Run the database
 docker-compose up test_db -d
 echo '🟡 - Waiting for database to be ready...'
 
-# go to current directory
-cd $DIR
-echo "$PWD"
-# Wait for the database to be ready
-$DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
+# go to wait-for-it.sh directory and run the script
+# Wait for the database to be ready before running the migrations
+$(cd "$(dirname "$0")" && pwd)/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 
-# go to the root of the project
-cd ..
-echo "$PWD"
 # Run the migrations
 npx prisma migrate reset --force
