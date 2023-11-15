d5f81a5307b3400e3c7fad0b565dd53e21d69221
Salih <salihozdemir94@gmail.com>
2023-11-15 15:50:08 +0300

    feat: add refresh token mechanism

--- apps/web/app/lib/axios.ts
+++ apps/web/app/lib/axios.ts
@@ -1,6 +1,13 @@
-import axios from "axios";
+import axios, { type AxiosError } from "axios";
+import { fetchers } from "~/lib/fetchers";
 
 const BASE_URL = "http://localhost:8080";
+
+export type ApiError = AxiosError<{
+  error: string;
+  message: string;
+  statusCode: number;
+}>;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -9,3 +16,38 @@ export const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// refresh tokens if 401
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async function (error: ApiError) {
+    const response = error?.response;
+    if (!response) {
+      return Promise.reject(error);
+    }
+
+    const status = response.status;
+
+    // API returns 403 when could not refresh tokens.
+    if (status === 403) {
+      if (window.location.pathname !== "/login") {
+        window.location.replace("/login");
+      }
+      return Promise.reject(error);
+    }
+
+    const originalRequest = error.config as typeof error.config & {
+      _retry?: boolean;
+    };
+
+    if (response.status === 401 && !originalRequest?._retry) {
+      originalRequest._retry = true;
+      await fetchers.refreshToken();
+      return axiosInstance(originalRequest);
+    }
+
+    return Promise.reject(error);
+  },
+);
