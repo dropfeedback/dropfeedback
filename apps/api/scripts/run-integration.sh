6adba3efcbe93027f5f5409a85caf7fc513a83d1
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-15 21:47:29 +0300

    fix(api): dir names

--- apps/api/scripts/run-integration.sh
+++ apps/api/scripts/run-integration.sh
@@ -7,12 +7,18 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 # Export env vars
 export $(grep -v '^#' .env.test | xargs)
 
+# go to the root of the project
+cd ..
 # Run the database
 docker-compose up test_db -d
 echo '🟡 - Waiting for database to be ready...'
 
+# go to current directory
+cd $DIR
 # Wait for the database to be ready
 $DIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
 
+# go to the root of the project
+cd ..
 # Run the migrations
 npx prisma migrate reset --force
