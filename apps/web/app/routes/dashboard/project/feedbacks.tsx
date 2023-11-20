227ba18cf7a178d62f7d215b5f6c58f2e164ede8
Salih <salihozdemir94@gmail.com>
2023-11-20 16:40:20 +0300

    feat: create basic feedbacks page

--- apps/web/app/routes/dashboard/project/feedbacks.tsx
+++ apps/web/app/routes/dashboard/project/feedbacks.tsx
@@ -1,3 +1,26 @@
+import { useParams } from "@remix-run/react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchers } from "~/lib/fetchers";
+
 export default function Feedbacks() {
-  return <div className="h-[200vh]">Feedbacks Table</div>;
+  const { projectId } = useParams<{ projectId: string }>();
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["feedbacks", { projectId }],
+    queryFn: () => fetchers.getFeedbacks({ projectId: projectId! }),
+    enabled: !!projectId,
+  });
+
+  if (isError) return null;
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Feedbacks</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
