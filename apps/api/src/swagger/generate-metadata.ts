58c6e8d917a2d5b1180c573b85674122148051ee
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-11-22 02:41:07 +0300

    chore(api): add swagger

---
+++ apps/api/src/swagger/generate-metadata.ts
@@ -0,0 +1,16 @@
+import { PluginMetadataGenerator } from '@nestjs/cli/lib/compiler/plugins';
+import { ReadonlyVisitor } from '@nestjs/swagger/dist/plugin';
+
+const generator = new PluginMetadataGenerator();
+generator.generate({
+  visitors: [
+    new ReadonlyVisitor({
+      introspectComments: true,
+      classValidatorShim: true,
+      pathToSource: __dirname,
+    }),
+  ],
+  outputDir: __dirname,
+  watch: false,
+  tsconfigPath: './tsconfig.json',
+});
