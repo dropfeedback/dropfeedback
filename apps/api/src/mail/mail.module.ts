9a9d788d8cca948f32fbeb6ac7b0e9debcfea547
melihkizmaz <melihkizmaz@icloud.com>
2023-11-25 21:24:16 +0300

    chore: unnecessary  ConfigService provider removed

--- apps/api/src/mail/mail.module.ts
+++ apps/api/src/mail/mail.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [JwtModule],
-  providers: [ConfigService, MailService],
+  providers: [MailService],
   exports: [MailService],
 })
 export class MailModule {}
