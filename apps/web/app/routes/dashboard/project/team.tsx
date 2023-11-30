1a9385cd0ea2a968691df1bb62015d09a1d5f26a
Salih <salihozdemir94@gmail.com>
2023-11-30 13:02:05 +0300

    chore: add skeleton for loading state

--- apps/web/app/routes/dashboard/project/team.tsx
+++ apps/web/app/routes/dashboard/project/team.tsx
@@ -7,6 +7,7 @@ import { Button } from "~/components/ui/button";
 import { Table, TableBody, TableCell, TableRow } from "~/components/ui/table";
 import { Separator } from "~/components/ui/separator";
 import { TeamInviteActions } from "~/components/team-invite-actions";
+import { TeamSkeleton } from "~/components/team-skeleton";
 import { fetchers } from "~/lib/fetchers";
 import { cn } from "~/lib/utils";
 import { MemberInviteState, type ProjectTeam } from "~/types";
@@ -21,8 +22,6 @@ export default function Teams() {
   });
 
   if (team.isError) return <p>Could not load team</p>;
-
-  if (team.isPending) return <p>Loading...</p>;
 
   return (
     <div className="h-full space-y-6 bg-background">
@@ -41,83 +40,89 @@ export default function Teams() {
       <div className="container">
         <div className="mb-6 rounded-lg border p-12">
           <div className="m-auto max-w-[500px] space-y-8">
-            <div className="rounded-sm border">
-              <h3 className="bg-accent py-2 pl-4 font-semibold text-muted-foreground">
-                Members
-              </h3>
-              <Table>
-                <TableBody>
-                  {team.data.members.map((member) => (
-                    <TableRow key={member.id}>
-                      <TableCell>
-                        <div className="flex items-center space-x-4">
-                          <Avatar>
-                            <AvatarImage
-                              src={member.avatarUrl}
-                              alt={`${member.firstName} ${member.lastName}`}
-                            />
-                            <AvatarFallback>
-                              {member.firstName[0] + member.lastName[0]}
-                            </AvatarFallback>
-                          </Avatar>
-                          <div>
-                            <p className="font-medium leading-none">
-                              {member.firstName} {member.lastName}
-                            </p>
-                            <p className="text-muted-foreground">
-                              {member.email}
-                            </p>
-                          </div>
-                        </div>
-                      </TableCell>
-                      <TableCell className="w-20 capitalize text-muted-foreground">
-                        {member.role}
-                      </TableCell>
-                      <TableCell className="w-9 text-right">
-                        <TeamMemberActions member={member} />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
-            <div className="rounded-sm border">
-              <h3 className="bg-accent py-2 pl-4 font-semibold text-muted-foreground">
-                Invites
-              </h3>
-              <Table>
-                <TableBody>
-                  {team.data.invites.map((invite) => (
-                    <TableRow key={invite.id}>
-                      <TableCell>{invite.email}</TableCell>
-                      <TableCell className="w-20 capitalize text-muted-foreground">
-                        {invite.role}
-                      </TableCell>
-                      <TableCell className="w-16 text-muted-foreground">
-                        <div
-                          className={cn(
-                            "inline-flex select-none items-center rounded-md px-2.5 py-0.5 text-xs font-semibold",
-                            {
-                              "bg-amber-foreground text-amber":
-                                invite.state === MemberInviteState.Pending,
-                              "bg-red-foreground text-red":
-                                invite.state === MemberInviteState.Rejected,
-                              "bg-green-foreground text-green":
-                                invite.state === MemberInviteState.Accepted,
-                            },
-                          )}
-                        >
-                          {invite.state}
-                        </div>
-                      </TableCell>
-                      <TableCell className="w-9 text-right">
-                        <TeamInviteActions invite={invite} />
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </div>
+            {team.isPending ? (
+              <TeamSkeleton />
+            ) : (
+              <>
+                <div className="rounded-sm border">
+                  <h3 className="bg-accent py-2 pl-4 font-semibold text-muted-foreground">
+                    Members
+                  </h3>
+                  <Table>
+                    <TableBody>
+                      {team.data.members.map((member) => (
+                        <TableRow key={member.id}>
+                          <TableCell>
+                            <div className="flex items-center space-x-4">
+                              <Avatar>
+                                <AvatarImage
+                                  src={member.avatarUrl}
+                                  alt={`${member.firstName} ${member.lastName}`}
+                                />
+                                <AvatarFallback>
+                                  {member.firstName[0] + member.lastName[0]}
+                                </AvatarFallback>
+                              </Avatar>
+                              <div>
+                                <p className="font-medium leading-none">
+                                  {member.firstName} {member.lastName}
+                                </p>
+                                <p className="text-muted-foreground">
+                                  {member.email}
+                                </p>
+                              </div>
+                            </div>
+                          </TableCell>
+                          <TableCell className="w-20 capitalize text-muted-foreground">
+                            {member.role}
+                          </TableCell>
+                          <TableCell className="w-9 text-right">
+                            <TeamMemberActions member={member} />
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+                <div className="rounded-sm border">
+                  <h3 className="bg-accent py-2 pl-4 font-semibold text-muted-foreground">
+                    Invites
+                  </h3>
+                  <Table>
+                    <TableBody>
+                      {team.data.invites.map((invite) => (
+                        <TableRow key={invite.id}>
+                          <TableCell>{invite.email}</TableCell>
+                          <TableCell className="w-20 capitalize text-muted-foreground">
+                            {invite.role}
+                          </TableCell>
+                          <TableCell className="w-16 text-muted-foreground">
+                            <div
+                              className={cn(
+                                "inline-flex select-none items-center rounded-md px-2.5 py-0.5 text-xs font-semibold",
+                                {
+                                  "bg-amber-foreground text-amber":
+                                    invite.state === MemberInviteState.Pending,
+                                  "bg-red-foreground text-red":
+                                    invite.state === MemberInviteState.Rejected,
+                                  "bg-green-foreground text-green":
+                                    invite.state === MemberInviteState.Accepted,
+                                },
+                              )}
+                            >
+                              {invite.state}
+                            </div>
+                          </TableCell>
+                          <TableCell className="w-9 text-right">
+                            <TeamInviteActions invite={invite} />
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
