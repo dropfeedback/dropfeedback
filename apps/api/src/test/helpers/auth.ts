b377af5e6379b2510ef40c768312d60eb92cb7fc
Alican Erdurmaz <alicanerdurmaz@gmail.com>
2023-12-04 02:20:10 +0300

    fix: tests

--- apps/api/src/test/helpers/auth.ts
+++ apps/api/src/test/helpers/auth.ts
@@ -1,18 +1,12 @@
 import { INestApplication } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import request from 'supertest';
+import * as bcrypt from 'bcrypt';
 
 export const MOCK_USER = {
   email: 'teste2e@teste2e.com',
   password: 'teste2e',
   fullName: 'teste2e',
-};
-
-export const signUp = async (app: INestApplication) => {
-  return await request(app.getHttpServer())
-    .post('/auth/local/signup')
-    .send({
-      ...MOCK_USER,
-    });
 };
 
 export const signIn = async (app: INestApplication) => {
@@ -23,8 +17,27 @@ export const signIn = async (app: INestApplication) => {
   });
 };
 
-export const getAuthCookie = async (app: INestApplication) => {
-  await signUp(app);
+export const getAuthCookie = async (
+  app: INestApplication,
+  prisma: PrismaClient,
+) => {
+  // create mock user
+  await prisma.user.upsert({
+    where: { email: MOCK_USER.email },
+    update: {},
+    create: {
+      email: MOCK_USER.email,
+      fullName: MOCK_USER.fullName,
+      UserProvider: {
+        create: {
+          type: 'internal',
+          emailVerified: true,
+          hash: await bcrypt.hash(MOCK_USER.password, 10),
+        },
+      },
+    },
+  });
+
   const res = await signIn(app);
   return res.header['set-cookie'];
 };
