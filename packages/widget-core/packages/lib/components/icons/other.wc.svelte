1579fbc4732a24017c7431c406ad871779d53b69
Salih <salihozdemir94@gmail.com>
2023-12-20 15:05:10 +0300

    fix: update the icons for more minimal design

--- packages/widget-core/packages/lib/components/icons/other.wc.svelte
+++ packages/widget-core/packages/lib/components/icons/other.wc.svelte
@@ -1,5 +1,16 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="none" viewBox="0 0 48 48">
-	<circle cx="9.5" cy="24.5" r="3.5" fill="var(--color-icon-inverse)" />
-	<circle cx="23.5" cy="24.5" r="3.5" fill="var(--color-icon-inverse)" />
-	<circle cx="37.5" cy="24.5" r="3.5" fill="var(--color-icon-inverse)" />
+<script>
+	export let size = 48;
+	$: sizePx = `${size}px`;
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	style:width={sizePx}
+	style:height={sizePx}
+	fill="currentColor"
+	viewBox="0 0 48 48"
+>
+	<circle cx="9.5" cy="24.5" r="3.5" />
+	<circle cx="23.5" cy="24.5" r="3.5" />
+	<circle cx="37.5" cy="24.5" r="3.5" />
 </svg>
