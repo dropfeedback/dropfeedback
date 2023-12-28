7167914b25510886ffa2205b01a52e6121642a45
Salih Özdemir <salihozdemir94@gmail.com>
2023-12-28 12:06:03 +0300

    refactor: team invite modal and team member actions (#68)

---
+++ packages/web/app/lib/constants/roles.ts
@@ -0,0 +1,18 @@
+export const ROLES = [
+  {
+    value: "owner",
+    name: "Owner",
+    description:
+      "Can add/remove members, change roles, and delete the project.",
+  },
+  {
+    value: "manager",
+    name: "Manager",
+    description: "Can add/remove members and change roles.",
+  },
+  {
+    value: "member",
+    name: "Member",
+    description: "Can view the project and its feedback.",
+  },
+];
