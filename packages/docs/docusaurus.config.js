df49b2e1fee255159c22782cd51124c93dd426cc
Salih <salihozdemir94@gmail.com>
2024-01-03 12:38:00 +0300

    docs: fix build issues

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -113,10 +113,6 @@ const config = {
             title: "More",
             items: [
               {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
                 label: "GitHub",
                 href: "https://github.com/facebook/docusaurus",
               },
