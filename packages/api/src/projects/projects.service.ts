d6be20d9c56317a068a50d66fb06ca96f8daa596
melihkizmaz <melihkizmaz@icloud.com>
2024-01-14 20:51:13 +0300

    feat: check project membership project helper method created

--- packages/api/src/projects/projects.service.ts
+++ packages/api/src/projects/projects.service.ts
@@ -560,4 +560,26 @@ export class ProjectsService {
       throw new BadRequestException(error.message);
     }
   }
+
+  async checkProjectMembership({
+    projectId,
+    userId,
+  }: {
+    projectId: string;
+    userId: string;
+  }) {
+    const projectMember = await this.prisma.projectMember.findUnique({
+      where: {
+        userId_projectId: {
+          userId,
+          projectId,
+        },
+      },
+    });
+
+    if (!projectMember) {
+      throw new BadRequestException('You are not a member of this project');
+    }
+    return projectMember;
+  }
 }
